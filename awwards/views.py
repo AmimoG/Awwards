0b38dafe8b6bb8284dea9f5be18a9339277dc966
AmimoG <gilbertmatete6@gmail.com>
2020-11-03 05:20:55 +0300

    post view function added

--- awwards/views.py
+++ awwards/views.py
@@ -3,10 +3,13 @@ from django.contrib.auth import login, authenticate
 from .forms import SignUpForm
 from django.shortcuts import render, redirect
 
+
+@login_required(login_url='login')
 def index(request):
     return render(request, 'award/index.html')
 
-def signup_view(request):
+@login_required(login_url='login')
+def signup(request):
     form = SignUpForm(request.POST)
     if form.is_valid():
         user = form.save()
@@ -23,3 +26,56 @@ def signup_view(request):
     else:
         form = SignUpForm()
     return render(request, 'registration/signup.html', {'form': form})
+
+@login_required(login_url='login')
+def edit_profile(request, username):
+    current_user = request.user
+    if request.method == 'POST':
+        try:
+            profile = Profile.objects.get(user=current_user)
+            form = UpdateUserProfileForm(request.POST,instance=profile)
+            if form.is_valid():
+                profile = form.save(commit=False)
+                profile.user = current_user
+                profile.save()
+            return redirect('index')
+        except:
+            form = UpdateUserProfileForm(request.POST)
+            if form.is_valid():
+                profile = form.save(commit=False)
+                profile.user = current_user
+                profile.save()
+            return redirect('index')
+    else:
+        if Profile.objects.filter(user=current_user):
+            profile = Profile.objects.get(user=current_user)
+            form = UpdateUserProfileForm(instance=profile)
+        else:
+            form = UpdateUserProfileForm()
+    return render(request, 'award/update_profile.html', {'form': form} )
+
+@login_required(login_url='login')
+def post(request):
+    current_user = request.user
+    try:
+        profile = Profile.objects.get(user = current_user)
+    except:
+        return redirect('edit_profile',username = current_user.username)
+
+    try:
+        posts = Post.objects.filter(neighborhood = profile.neighborhood)
+    except:
+        posts = None
+
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.user=current_user
+            post.neighborhood = profile.neighborhood
+            post.save()
+        return redirect('index')
+    else:
+        form = PostForm()
+   
+    return render(request, 'award/post.html', {'posts': posts, 'form': form})
