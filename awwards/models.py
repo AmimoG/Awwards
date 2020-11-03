34a82e14d7ce04d665f7ade261690c347d782ffd
AmimoG <gilbertmatete6@gmail.com>
2020-11-03 13:22:19 +0300

    post models added

--- awwards/models.py
+++ awwards/models.py
@@ -1,42 +1,79 @@
 from django.db import models
 from django.contrib.auth.models import User
+import datetime
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from pyuploadcare.dj.models import ImageField
 
-class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    first_name = models.CharField(max_length=100, blank=True)
-    last_name = models.CharField(max_length=100, blank=True)
-    email = models.EmailField(max_length=150)
-    bio = models.TextField()
 
-    def __str__(self):
-        return self.user.username
 
-@receiver(post_save, sender=User)
-def update_profile_signal(sender, instance, created, **kwargs):
-    if created:
-        Profile.objects.create(user=instance)
-    instance.profile.save()
-    
-
-class Post(models.Model):
-    title = models.CharField(max_length=155)    
-    url = models.URLField(max_length=255)
-    description = models.TextField(max_length=255)
-    image = models.ImageField(upload_to='images/')      
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")    
-    
-    def __str__(self):
-        return f'{self.title}'
-
-    def delete_post(self):
-        self.delete()
+class Image(models.Model):
+    image = models.ImageField(upload_to='images/')
+    name = models.CharField(max_length=60)
+    description = models.TextField()
+    author = models.CharField(max_length=40, default='admin')
+    date = models.DateTimeField(auto_now_add=True)
+        
 
     @classmethod
-    def all_posts(cls):
-        return cls.objects.all()
+    def filter_by_location(cls, location):
+        image_location = Image.objects.filter(location__name=location).all()
+        return image_location
 
-    def save_post(self):
+    @classmethod
+    def update_image(cls, id, value):
+        cls.objects.filter(id=id).update(image=value)
+
+    @classmethod
+    def get_image_by_id(cls, id):
+        image = cls.objects.filter(id=id).all()
+        return image
+
+    @classmethod
+    def search_by_category(cls, category):
+        images = cls.objects.filter(category__name__icontains=category)
+        return images
+
+    def __str__(self):
+        return self.name
+
+    def save_image(self):
         self.save()
+
+    def delete_image(self):
+        self.delete()
+
+    class Meta:
+        ordering = ['date']
+
+class Profile(models.Model):
+	user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+	first_name = models.CharField(max_length=50, null=True, blank=True)
+	last_name = models.CharField(max_length=50, null=True, blank=True)
+	location = models.CharField(max_length=50, null=True, blank=True)
+	url = models.CharField(max_length=80, null=True, blank=True)
+	profile_info = models.TextField(max_length=150, null=True, blank=True)
+	created = models.DateField(auto_now_add=True)
+	picture = models.ImageField(blank=True, null=True, verbose_name='Picture')
+
+	def save(self, *args, **kwargs):
+		super().save(*args, **kwargs)
+		SIZE = 250, 250
+
+		# if self.picture:
+		# 	pic = Image.open(self.picture.path)
+		# 	pic.thumbnail(SIZE, Image.LANCZOS)
+		# 	pic.save(self.picture.path)
+
+	def __str__(self):
+		return self.user.username
+
+class Comments (models.Model):
+    comment_post = models.CharField(max_length=150)
+    author = models.ForeignKey('Profile',related_name='commenter' , on_delete=models.CASCADE)
+    commented_image = models.ForeignKey('Image', on_delete=models.CASCADE)
+    date = models.DateTimeField(auto_now_add=True)
+
+    '''Method to filter database results'''
+    def __str__(self):
+        return self.author
+
