f2f5c1bd3f8f288d9f87f14afa1a6d965e9b23b1
AmimoG <gilbertmatete6@gmail.com>
2020-11-03 04:55:18 +0300

    profile model added

--- awwards/views.py
+++ awwards/views.py
@@ -1,7 +1,22 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
+from .forms import SignUpForm
 
 # Create your views here.
 
 def index(request):
     
     return render(request, 'award/index.html')
+
+def signup_view(request):
+    form = SignUpForm(request.POST)
+    if form.is_valid():
+        form.save()
+        username = form.cleaned_data.get('username')
+        password = form.cleaned_data.get('password1')
+        user = authenticate(username=username, password=password)
+        login(request, user)
+        return redirect('home')
+    else:
+        form = SignUpForm()
+    return render(request, 'signup.html', {'form': form})
